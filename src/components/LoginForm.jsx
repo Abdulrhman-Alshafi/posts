20a7feec92937a616f9b1e2f091f86417b20edfe
Abdulrhman-Alshafi <Abdulrahman.Alshafi@optimumpartners.com>
2025-11-11 08:15:53 +0300

    removing react import

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styles from "./LoginForm.module.css";
 const LoginForm = ({ users, onLogin }) => {
   const [email, setEmail] = useState("");
